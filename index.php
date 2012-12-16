52001e2bd601ee0528cae9fc5dd19ea4bae460b1
Dylan Lloyd <dylan@dylansserver.com>
2012-12-16 11:00:44 -0500

    added some controls, reset doesn't work
    some problem with the object copy `automaton = seed`

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
       <canvas width='750px' height='750px' id='canvas'></canvas>
       <div id='controls'>
         <h1>2D</h1>
+        <div id='start-automaton' class='control'>Start automaton</div>
+        <div id='stop-automaton' class='control'>Stop automaton</div>
+        <div id='reset-automaton' class='control'>Reset automaton</div>
+        <div id='highlight-seed' class='control'>Highlight seed</div>
+        <br>
         Generation: <span id='generation'>0</span>
       </div>
     </div>
