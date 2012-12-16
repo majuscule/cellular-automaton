62b73ae19321e18709f44efd9d290ef0722b04d8
Dylan Lloyd <dylan@dylansserver.com>
2012-12-16 04:32:07 -0500

    whoops! typos

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     <title>cellular automaton</title>
     <link type='text/css' rel='stylesheet' href='style.css'>
     <script type='text/javascript' src='jquery-core.js'></script>
-    <script type='text/javascript' src='2d.js'></script>
+    <script type='text/javascript' src='1d.js'></script>
   </head>
   <body>
     <div id='container'>
       <canvas id='canvas'></canvas>
     </div>
     <ul id='controls'>
-	  <li id='draw' class='tool'>2D</li>
+	  <li id='draw' class='tool'>1D</li>
     </ul>
     <div id='markup'>test</div>
   </body>
