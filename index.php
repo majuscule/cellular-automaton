5e9835296b4f407f759f0c165f2d33500a73f2ce
Dylan Lloyd <dylan@dylansserver.com>
2012-12-16 09:49:43 -0500

    style changes

--- index.php
+++ index.php
@@ -8,11 +8,10 @@
   <body>
     <div id='container'>
       <canvas width='750px' height='750px' id='canvas'></canvas>
+      <div id='controls'>
+        <h1>2D</h1>
+        Generation: <span id='generation'>0</span>
+      </div>
     </div>
-    <div id='controls'>
-    <h1>2D</h1>
-    Generation: <span id='generation'>0</span>
-    </div>
-    <div id='markup'>test</div>
   </body>
 </html>
