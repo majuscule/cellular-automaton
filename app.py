34da55d138e70ae1f331ed99ea50d9e4ad5cc6fc
Robert Picard <mail@robert.io>
2012-12-27 05:05:24 -0500

    Finish first draft of flask app

--- app.py
+++ app.py
@@ -1,20 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import redis
 
 app = Flask(__name__)
+app.debug = True
 
-# r = redis
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def generate_token():
+    size = random.randint(4,10)
+
+    token = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(size))
+
+    if r.get(token):
+        token = generate_token()
+
+    return token
+
 
 @app.route('/', defaults={'token':0})
 @app.route('/<int:token>')
 def index(token):
     if token == 0:
-        return render_template('index.html'), seed=0)
+        return render_template('index.html', seed=0)
 
-    integer = r.get(token)
-    seed = bin(integer)[2:]
+    seed_int = r.get(token)
+    if not seed_int:
+        return render_template('index.html', seed=0)
+
+    seed = bin(seed_int)[2:]
     return render_template('index.html', seed=seed)
 
+
+@app.route('/_make_permalink')
+def make_permalink():
+    seed = request.form['seed']
+
+    seed_int = int(seed, 2)
+
+    token = generate_token()
+
+    r.set(token, seed_int)
+
+    return jsonify(token)
 
 if __name__ == '__main__':
     app.run()
